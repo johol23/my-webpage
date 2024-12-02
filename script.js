e7eec202c405e6921b0096116d42fce6bcbf66f3
johol23 <143049332+johol23@users.noreply.github.com>
2024-12-02 14:23:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const data = {
     content: "This is a simple template engine demonstration.",
     author: "Dr.Red",
 };
+
+function renderTemplate(template, data) {
+    for (const key in data) {
+        const regex = new RegExp('{{' + key + '}}', 'g');
+        template = template.replace(regex, data[key]);
+    }
+    return template;
+}
+const template = `
+    <h3>{{title}}</h3>
+    <p>{{content}}</p>
+    <p>Author: {{author}}</p>
+`;
+const app = document.getElementById('app');
+app.innerHTML = renderTemplate(template, data);
